2bf5f34f37aaf3105f165597bb9c9aee8be71ffc
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-02-09 07:25:26 +0800

    Add files via upload

--- UI.js
+++ UI.js
@@ -706,13 +706,9 @@ ui.layout(
                            </vertical>
                          </horizontal>
                        </vertical>
-                       <horizontal>
-                       <vertical>
-                       <text w="auto" textColor="#999999" textSize="12sp" text="已获得卡密，切已保存卡密的，（脚本配置好）直接点击此处‘VIP登录’，再点‘开始学习’" />
-                       </vertical>
+                        <text w="auto" textColor="#999999" textSize="12sp" text="已获得卡密，切已保存卡密的，（脚本配置好）直接点击此处‘VIP登录’，再点‘开始学习’" />
                         <button id="denglu_1" text="VIP登录--1" textSize="18sp" color="#ffffff" bg="#FF4FB3FF" layout_weight='1'></button>
-                        <button h="50" layout_gravity="center" id="log" textSize="16sp" text="查看日志" layout_weight='1'/>
-                        </horizontal>
+                        <button h="50" layout_gravity="center" id="log" textSize="18sp" text="查看日志" layout_weight='1'></button>
                         <button h="60" layout_gravity="center" id="update" textSize="18sp" /> 
                         <button id="start" text="开 始 学 习--2" textSize="25sp" color="#ffffff" bg="#FF4FB3FF" foreground="?selectableItemBackground"/>
                     </vertical>

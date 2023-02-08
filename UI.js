686d810c9d893fe8c36011eee5b147322a55b24e
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-02-08 18:04:05 +0800

    Add files via upload

--- UI.js
+++ UI.js
@@ -1242,6 +1242,7 @@ ui.denglu.click(function() {
 });
 //创建按键的点击事件
 ui.cun_bh_kami.on('click', () => {
+    let kami = ui.bh_kami.text();
     if (kami != "" && kami != null) ui.storage.put("bh_kami", ui.bh_kami.text());
     else  toast('请正确输入卡密或联系群主');
  });

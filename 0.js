260907162fbb261e80ae9f529219fed3a4ee9272
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-02-09 17:26:00 +0800

    Add files via upload

--- 0.js
+++ 0.js
@@ -705,7 +705,7 @@ if(meizhou_dao != null) meizhou_dao.click()
   textMatches(/.*月|发现新版本/).waitFor();
   if (text("发现新版本").exists()) return fError("有弹窗无法每周答题，可使用旧版修改版本号版取消弹窗"), sleep(1000), text("取消").findOne().click(), sleep(1000), back(), text("我要答题").waitFor(),
     sleep(1000), back(), ran_sleep(), !0;
-  let scoll = depth(21).scrollable().findOne();
+  let scoll = depth(22).scrollable().findOne();
   // 下面是倒叙作答
   if (meizhou_dao) {
     fInfo("倒序查找未做题目");
@@ -713,6 +713,11 @@ if(meizhou_dao != null) meizhou_dao.click()
     while (!text("已作答").exists()) {
       scoll.scrollForward();
       sleep(300);
+      let dixian_slt = text("您已经看到了我的底线").filter(function (w) {
+        log("底线：", w.bounds().top, device_h);
+        return w.bounds().top <= device_h - 30;
+      });
+      if (dixian_slt.exists()) {fInfo("每周答题全部已作答。"); break;}
     }
     var clt = text("未作答").find();
     if (clt.empty()) return fInfo("每周答题全部已作答。"), ran_sleep(), back(), text("每周答题").waitFor(),

e4a8144cd2436a665d6d9261425d4b3e49fb49e6
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-02-11 02:25:13 +0800

    Add files via upload

--- 0.js
+++ 0.js
@@ -686,8 +686,8 @@ function do_meiri() {
 
 /********每周答题*********/
 function do_meizhou() {
- let meizhou_dao = text("每周答题").findOne(4000);
-if(meizhou_dao != null) meizhou_dao.click()
+ let meizhou_d = text("每周答题").findOne(4000);
+if(meizhou_d != null) meizhou_dao.parent().click()
   else{var textOrder = text("排行榜").findOnce().parent();
     while (text("排行榜").exists()) {
         console.info("点击每周答题");

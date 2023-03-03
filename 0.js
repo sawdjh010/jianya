e933b15902a144a00540258572c56af216a62190
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-03-04 01:04:06 +0800

    Add files via upload

--- 0.js
+++ 0.js
@@ -497,9 +497,9 @@ function do_wenzhang() {
   }
   log("切换地区");
   //let beijing_1 =className("android.widget.TexitView").depth(17).find();
-  queryList_1(find(),"北京");
+ // queryList_1(find(),"北京");
   //className("android.widget.TexitView").depth(17).findOne(2000)
-  text("切换地区").findOne().click();
+  text("切换地区").findOne(3000).click();
   log("查找北京");
   text("北京").waitFor();
   sleep(500);exit

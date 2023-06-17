e64fe518a56c5250a764c9c038b8e97f1f2ecd00
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-06-18 00:58:20 +0800

    Add files via upload

--- UI.js
+++ UI.js
@@ -1227,7 +1227,7 @@ toast(tskami);
  if(tskami!=null||tskami!='') {var targetDate = shijian_yanzheng[tskami];
  var diff = parseInt((targetDate - Date.now()) / 1000);
  //toast(diff);
- toast(Date.now().getTime() + ' '+ targetDate);
+ toast((Date.now()).getTime() + ' '+ targetDate);
  //console.log(diff);
  }else var diff = -1;
 // //  let hour = parseInt(diff / 3600);
